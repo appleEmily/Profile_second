ca934696f5235f6d6846512404275baf928d0fee
Emily Nozaki <emiri3128.gmail.com>
2022-02-13 16:05:54 +0900

    おわた

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
+        
     }
-
-
+    
+    @IBAction func name(_ sender: Any) {
+        
+        image.image = UIImage(named: "philImage")
+        
+        titleLabel.text = "名前"
+        
+        label.text = "僕はフィル"
+    }
+    
+    @IBAction func sport(_ sender: Any) {
+        image.image = UIImage(named: "trackImage")
+        
+        titleLabel.text = "好きなスポーツ"
+        
+        label.text = "僕は陸上が好き！！！！！！！"
+        
+    }
+    
+    @IBAction func food(_ sender: Any) {
+        
+        image.image = UIImage(named: "appleImage")
+        
+        titleLabel.text = "好きな食べ物"
+        
+        label.text = "僕はりんごになりたい！！！！！！！"
+    }
+    
+    @IBAction func hobby(_ sender: Any) {
+        image.image = UIImage(named: "flightImage")
+        
+        titleLabel.text = "趣味"
+        
+        label.text = "趣味は飛行機"
+        
+    }
+    
 }
 
